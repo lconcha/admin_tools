ddca8e21d22237268b936a2236f7c04e36b5ecce
Luis Concha <lconcha@gmail.com>
2023-08-17 11:50:20 -0600

    first

---
+++ fmrilab_backup_homes_in_server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+
+servername="hahn"
+if [ "$HOSTNAME" != "$servername" ]
+then
+  echo "Can only run in $servername"
+  exit
+fi
+
+
+rsync -av --progress  --partial \
+  /mnt/raidSSD/* \
+  /mnt/home.bak/home/
