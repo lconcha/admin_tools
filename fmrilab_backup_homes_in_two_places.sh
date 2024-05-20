8cecfeb5859ee3da2c166c48657a07febbf18a6e
Luis Concha <lconcha@gmail.com>
2024-05-20 11:03:14 -0600

    make sure we find scripts

--- fmrilab_backup_homes_in_two_places.sh
+++ fmrilab_backup_homes_in_two_places.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+
+scripts_dir=$(dirname $0)
+echo "scripts_dir : $scripts_dir"
+
+
 echo "Begin backup to two places"
 date
 
-fmrilab_backup_homes_in_server.sh
-fmrilab_backup_homes_in_sesamo.sh
+${scripts_dir}/fmrilab_backup_homes_in_server.sh
+${scripts_dir}/fmrilab_backup_homes_in_sesamo.sh
 
 date
 echo "Finished backup to two places"
