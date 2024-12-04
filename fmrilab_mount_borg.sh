f3e8f94cf7c7f69262cc7e3a73590bccb4ebdb94
Luis Concha <lconcha@gmail.com>
2024-12-04 10:24:57 -0600

    first

---
+++ fmrilab_mount_borg.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+export BORG_REPO=admin@sesamo:/volume1/fmrilab/backup/borg_repo
+export BORG_PASSPHRASE=$(cat /misc/lauterbur/lconcha/code/admin_tools/private/borg_passphrase_sesamo5)
+#export BORG_PASSPHRASE=$(cat `dirname $0`/private/borg_passphrase_sesamo5)
+
+#export BORG_REPO=admin@sesamo:/volume1/fmrilab/tests/repo.test
+#export BORG_PASSPHRASE=test
+
+
+# Checar si soy sudo
+if [ ! $(id -u) -eq 0 ]; then
+        echo "FATAL ERROR. Only sudo can do this"
+        exit 2
+fi
+
+
+
+if [[ "${HOSTNAME}" == "tesla" ]]
+then
+  echo "This is tesla"
+else
+  echo "FATAL ERROR. Can only mount on tesla"
+  exit 2
+
+fi
+
+
+local_mount_point=/bak.misc
+
+if [ ! -d $local_mount_point ]
+then
+  echo "FATAL ERROR. Local mount point does not exist: $local_mount_point"
+  exit 2
+fi
+
+
+
+borg  \
+  --remote-path=/usr/local/bin/borg \
+  mount $BORG_REPO \
+  -o allow_other,ro \
+  $local_mount_point
