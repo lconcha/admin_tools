159246db00e7273042c4eb8a7619a9ab0ff5d8b6
Luis Concha <lconcha@gmail.com>
2025-08-21 10:32:23 -0600

    added option to umount

--- fmrilab_mount_borg.sh
+++ fmrilab_mount_borg.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 
-export BORG_REPO=admin@sesamo:/volume1/fmrilab/backup/borg_repo
-export BORG_PASSPHRASE=$(cat `dirname $0`/private/borg_passphrase_sesamo5)
 
 
 
@@ -23,6 +21,32 @@ else
 
 fi
 
+option=$1
+case "$option" in
+  "")
+    umount_only=0
+    echo "No option given."
+  ;;
+  "umount_only")
+    umount_only=1
+    echo "Will only umount"
+  ;;
+  *)
+    echo "ERROR: Unrecognized argument: $option"
+    exit 2
+  ;;
+esac
+
+
+export BORG_REPO=admin@sesamo:/volume1/fmrilab/backup/borg_repo
+export BORG_PASSPHRASE=$(cat `dirname $0`/private/borg_passphrase_sesamo5)
+if [ -z "$BORG_REPO" ] || [ -z "$BORG_PASSPHRASE" ]; then
+  echo "FATAL ERROR. BORG_REPO or BORG_PASSPHRASE not set."
+  exit 2
+fi
+
+
+
 
 local_mount_point=/bak.misc
 
@@ -36,6 +60,13 @@ fi
 # Unmount first, then mount it again (to refresh)
 echo "Unmounting $local_mount_point ..."
 umount -fl $local_mount_point
+
+if [ $umount_only -eq 1 ]
+then
+  echo "Finished unmounting, exiting now."
+  exit 0
+fi
+
 
 # now mount...
 echo "Mounting borg in $local_mount_point (this takes a few minutes) ..."
